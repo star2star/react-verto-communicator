5a1918872d690289c6bc38aec2dcc22be525a154
Tom <Tom@FLSARMAC1542.local>
2016-05-23 17:09:32 -0400

    working toward adding store and internationalization, appbar container, etc.

---
+++ src/containers/appbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Radium from 'radium';
+//import ReactTooltip from 'react-tooltip';
+
+class AppBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={};
+  }
+
+  componentWillMount() {
+  }
+
+
+  render() {
+    // app bar styles
+    const styles = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+      order: '0',
+      backgroundColor: '#0544a4',
+      width: '100%',
+      color: '#FFFFFF',
+      height: '70px'
+      };
+
+
+    return (
+      <div>
+        <div className="appbar" style={styles}>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+AppBar.contextTypes = {
+  store: React.PropTypes.object
+};
+
+export default Radium(AppBar);
