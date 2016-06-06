03e18ffd5abbc25dca94390f4ec0e30f75a701c6
Cory Schimmoeller <Cory@FLSARMAC1499.local>
2016-06-06 10:31:50 -0400

    finished the about tests

--- src/tests/aboutComp-test.js
+++ src/tests/aboutComp-test.js
@@ -1,22 +1,51 @@
 import React from 'react';
-//import { shallow, mount, render } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import sinon from 'sinon';
 import ReactDOM from 'react-dom';
 import { mountWithIntl, shallowWithIntl } from '../helpers/intl-enzyme-test-helper.js';
-
 import About from '../components/about';
 
 jest.unmock('../components/about.js');
-
 jest.unmock('../helpers/intl-enzyme-test-helper.js');
 jest.unmock('../js/messages.js');
 
 
 describe('Default test for about', ()=>{
-  it('renders SignalFullIconSVG if network data is considered good', () => {
+
+  it('renders the correct number of elements by counting divs', () => {
     const wrapper = shallowWithIntl(<About cbClose={()=>{}}/>);
     expect(wrapper.find('div').length).toEqual(5);
   });
-  //test if it comes to the correct number of statements(divs)
-  //test that the props are actually getting used using find
+
+  it('assigns the correct value to version, depending on what is passed in', () => {
+    const wrapper = mountWithIntl(<About version='3.9.236' cbClose={()=>{}}/>);
+    expect(wrapper.props().version).toEqual('3.9.236');
+  });
+
+  it('assigns the correct value to gitRev, depending on what is passed in', () => {
+    const wrapper = mountWithIntl(<About gitRev='LRE785' cbClose={()=>{}}/>);
+    expect(wrapper.props().gitRev).toEqual('LRE785');
+  });
+
+  it('renders the images', () => {
+     const wrapper = shallowWithIntl(<About cbClose={()=>{}}/>);
+     expect(wrapper.find('img').length).toEqual(2);
+  });
+
+  it('renders a modal', () => {
+     const wrapper = shallowWithIntl(<About cbClose={()=>{}}/>);
+     expect(wrapper.find('Modal').length).toEqual(1);
+  });
+
+  it('the component has 3 internationalized messages', () => {
+     const wrapper = shallowWithIntl(<About cbClose={()=>{}}/>);
+     expect(wrapper.find('FormattedMessage').length).toEqual(3);
+  });
+
+  it('prints the gitRev and version props in line with their FormattedMessages', () => {
+     const wrapper = shallowWithIntl(<About cbClose={()=>{}}/>);
+     expect(wrapper.find('span').length).toEqual(2);
+  });
+
+
 });
