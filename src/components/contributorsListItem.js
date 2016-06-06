d7d8511a90c035ebbbb6ee0fb3719f24dcd5053a
Cory Schimmoeller <Cory@FLSARMAC1499.local>
2016-06-06 18:09:12 -0400

    end of the day check in, if you are reading this I hope you have a nice day.

--- src/components/contributorsListItem.js
+++ src/components/contributorsListItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import VertoBaseComponent from './vertobasecomponent';
-
+import Radium from 'radium';
 
 const propTypes = {
   compStyle: React.PropTypes.object,
@@ -18,11 +18,49 @@ class ContributorsList extends VertoBaseComponent{
   getCompStyle() {
     return this.props.compStyle;
   }
-  
+
+  getDefaultStyle(styleName) {
+      const styles = {
+
+            totalStyle: {
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-around",
+              flexWrap: "wrap",
+            },
+
+            imageStyle: {
+              borderRadius: "50%"
+            },
+
+            infoStyle: {
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              width: "50%"
+            },
+
+            nameStyle: {
+              fontSize: 20,
+              paddingBottom: "5px"
+            }
+      };
+      return (styles[styleName]);
+  }
+
+
   render() {
     return (
-      <div>
-        <img src={this.props.avatar} />
+      <div style={this.getStyle('totalStyle')}>
+          <span >
+            <img src={this.props.avatar} style={this.getStyle('imageStyle')}/>
+          </span>
+          <span style={this.getStyle('infoStyle')}>
+            <span style={this.getStyle('nameStyle')}>
+              {this.props.name} <br />
+            </span>
+              {this.props.email}
+          </span>
       </div>
     );
   }
@@ -31,4 +69,4 @@ class ContributorsList extends VertoBaseComponent{
 
 ContributorsList.propTypes = propTypes;
 
-export default ContributorsList;
+export default Radium(ContributorsList);
