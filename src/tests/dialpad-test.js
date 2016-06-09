66ae6bd5e19491557374f012dddc148a6c212cc7
knicholsc <knichols@star2star.com>
2016-06-09 15:34:39 -0400

    added classNames to dialpad.js and tests to dialpad-test

--- src/tests/dialpad-test.js
+++ src/tests/dialpad-test.js
@@ -4,47 +4,54 @@ import { shallow, mount, render } from 'enzyme';
 import sinon from 'sinon';
 import ReactDOM from 'react-dom';
 import {StyleRoot} from 'radium';
+//import CallHistory from '../components/callHistory.js';
 
 import Dialpad from '../components/dialpad.js';
 
 jest.unmock('../components/dialpad.js');
+jest.unmock('../components/callHistory.js');
 
 describe('<Dialpad />', ()=>{
 const lastCall = "9413569660";
 const nbrToDial = "9045299947";
+const a = '';
 const cbCall = function() {
   return 'cbCall executed';
 };
 
-  // input focused ? inputFocused state changed to true
-  //it('changes state to true on focus', () => {
-    // const wrapper = mount(<StyleRoot><Dialpad lastCall={lastCall}  nbrToDial={nbrToDial} cbCall={cbCall} /></StyleRoot>);
-    // wrapper.setState({inputFocused: true});
-    // //console.log('********************', wrapper.children().first().props());
-    // //console.log('$$$$$$$$$$$$$$$$$$$$$$', wrapper.children().first().find('.input'));
-    // const input = wrapper.children().first().find('.input');
-    // input.simulate('focus');
-    // expect(wrapper.state().inputFocused).toEqual(true);
-    // });
-    //
-    // it('changes state to false on blur', () => {
-    //   const wrapper = mount(<StyleRoot><Dialpad lastCall={lastCall}  nbrToDial={nbrToDial} cbCall={cbCall} /></StyleRoot>);
-    //   wrapper.setState({inputFocused: true});
-    //   const input = wrapper.children().first().find('.input');
-    //   console.log(input);
-    //   input.simulate('blur', { target: { inputFocused: false } });
-    //   expect(wrapper.state().inputFocused).toEqual(false);
-    //   });
-
-    it('accepts that it is being clicked', () => {
+    it('erases when back icon is clicked', () => {
       const onFocusStub = sinon.spy();
       const wrapper = mount(<StyleRoot><Dialpad lastCall={lastCall}  nbrToDial={nbrToDial} cbCall={cbCall} /></StyleRoot>);
       const backIcon = wrapper.children().first().find('.back');
-      backIcon.simulate('click');
-      //console.log(backIcon.debug());
+      const inputArea = wrapper.children().first().find('.input');
 
+      //console.log(inputArea.props());
       backIcon.simulate('click');
       expect(onFocusStub.calledOnce, true);
+      expect(inputArea.props().value).toEqual('904529994');
+      //console.log(inputArea.props());
+    });
+
+   it('displays lastNumber if number input is empty and dial is clicked.', () => {
+      const onClickStub = sinon.spy();
+      const wrapper = mount(<StyleRoot><Dialpad lastNumber={lastCall}  nbrToDial={a} cbCall={cbCall} /></StyleRoot>);
+      const dial = wrapper.children().first().find('.dial');
+      const inputArea = wrapper.children().first().find('.input');
+
+      dial.simulate('click');
+      expect(onClickStub.calledOnce, true);
+      expect(inputArea.props().value).toEqual('9413569660');
       });
+
+    // it('triggers a click event on callhistory icon', () => {
+    //    const onClickStub = sinon.spy();
+    //    const wrapper = mount(<StyleRoot><Dialpad lastNumber={lastCall}  nbrToDial={a} cbCall={cbCall} /></StyleRoot>);
+    //    const callhist = (wrapper.children().first().find('.callhist'));
+    //
+    //    callhist.simulate('click');
+    //    expect(onClickStub.calledOnce, true);
+    //    });
+
+
 
 });
