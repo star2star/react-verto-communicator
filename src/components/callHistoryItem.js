c413cbf3f0f06deb45f9c8820f132207d897ec0c
knicholsc <knichols@star2star.com>
2016-06-09 17:37:16 -0400

    initial styles for callHistoryItem

--- src/components/callHistoryItem.js
+++ src/components/callHistoryItem.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import VertoBaseComponent from './vertobasecomponent';
+//import moment from 'moment.js';
+import { UpArrowIconSVG, DownArrowIconSVG, MenuIconSVG } from './svgIcons';
+import Radium from 'radium';
+import {injectIntl, formattedMessage } from 'react-intl';
 
 const propTypes = {
   compStyle : React.PropTypes.object,
@@ -19,15 +23,39 @@ class CallHistoryItem extends VertoBaseComponent {
     const styles = {
       container: {
         display: 'flex',
+        color: '#4a4a4a',
+        marginTop: '20px',
+        marginBottom: '20px',
+        backgroundColor: '#fff',
         flex: 1,
+        fontWeight: '300',
+        minWidth: '375px',
         alignItems: "center",
-        justifyContent: 'flex-start',
-        flexDirection: "column",
+        cursor: 'pointer',
         maxWidth: "500px",
-        boxShadow: '0 16px 28px 0 rgba(0,0,0,.22),0 25px 55px 0 rgba(0,0,0,.21)',
-        backgroundColor: "green",
-        color: "yellow"
+        boxShadow: '0 16px 28px 0 rgba(0,0,0,.22),0 25px 55px 0 rgba(0,0,0,.21)'
       },
+      top: {
+        display: 'flex',
+        flexDirection: 'column',
+        fontSize: '30px',
+        color: '#26ccda',
+        justifyContent: 'space-between'
+      },
+      dirSVG : {
+        width: '30px',
+        height: '30px',
+        fill : '#009688'
+      },
+      menuCont : {
+        marginLeft: 'auto'
+      },
+      timestamp : {
+        marginTop: '15px',
+        fontSize: '11px',
+        color: '#4a4a4a',
+      }
+
      };
 
     return (styles[styleName]);
@@ -35,12 +63,36 @@ class CallHistoryItem extends VertoBaseComponent {
 
   render(){
 
+    var renderedDirection;
+    if(this.props.data.lastDirection == 'outgoing') {
+      renderedDirection = (<span className="outgoing">
+        <UpArrowIconSVG
+          svgStyle={{...this.getDefaultStyle('dirSVG')}}
+        />
+      </span>);
+    } else {
+      renderedDirection = (<span className="incoming">
+        <DownArrowIconSVG
+          svgStyle={{...this.getDefaultStyle('dirSVG')}}
+        />
+      </span>);
+    }
+
     return (
       <div style={this.getStyle('container')}>
-        <span>{this.props.data.callerId}</span>
-        <span>{this.props.data.lastTimestamp}</span>
-        <span>{this.props.data.nbrCalls}</span>
-        <span>{this.props.data.lastDirection}</span>
+        <div style={{...this.getDefaultStyle('top')}}>
+            <div>
+              {renderedDirection}
+              {this.props.data.callerId} ({this.props.data.nbrCalls})
+            </div>
+            <div style={{...this.getDefaultStyle('timestamp')}}>
+              {this.props.data.lastTimestamp}
+            </div>
+        </div>
+        <span style={{...this.getDefaultStyle('menuCont')}}>
+          <MenuIconSVG svgStyle={{...this.getStyle('dirSVG')}} />
+        </span>
+
       </div>);
   }
 
