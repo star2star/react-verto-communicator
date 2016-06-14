2d82d91669d4c7b0076fb957158898f375faff7c
Duncan Neal <dneal@star2star.com>
2016-06-14 13:50:07 -0400

    settingsCheckbox testing

--- src/tests/settingsCheckbox-test.js
+++ src/tests/settingsCheckbox-test.js
@@ -11,39 +11,91 @@ import SettingsCheckbox from '../components/settingsCheckbox.js';
 describe('settingsCheckbox Component', ()=>{
 
 const label="Use Video";
-
 const checkedOption={name:"useVideo", value: true};
-
 const cbSubmitSetting=sinon.spy();
 
 it('renders a input tag', () => {
   //expect(true).toBe(true);
    const wrapper = shallow(
-     <SettingsCheckbox checkedOption={checkedOption} cbSubmitSetting={cbSubmitSetting} label={label} />);
+     <SettingsCheckbox
+         checkedOption={checkedOption}
+         cbSubmitSetting={cbSubmitSetting}
+         label={label}
+      />);
    expect(wrapper.find('input').length).toEqual(1);
  });
 
   it('renders a div', () => {
     // expect(true).toBe(true);
      const wrapper = shallow(
-        <SettingsCheckbox checkedOption={checkedOption} cbSubmitSetting={cbSubmitSetting} label={label} />);
+       <SettingsCheckbox
+           checkedOption={checkedOption}
+           cbSubmitSetting={cbSubmitSetting}
+           label={label}
+        />);
      expect(wrapper.find('div').length).toEqual(1);
   });
 
   it('renders a span', () => {
     // expect(true).toBe(true);
      const wrapper = mount(
-        <SettingsCheckbox checkedOption={checkedOption} cbSubmitSetting={cbSubmitSetting} label={label} />);
+       <SettingsCheckbox
+           checkedOption={checkedOption}
+           cbSubmitSetting={cbSubmitSetting}
+           label={label}
+        />);
      expect(wrapper.find('span').length).toEqual(1);
    });
 
    it('renders a label', () => {
      // expect(true).toBe(true);
       const wrapper = mount(
-          <SettingsCheckbox checkedOption={checkedOption} cbSubmitSetting={cbSubmitSetting} label={label} />);
+        <SettingsCheckbox
+            checkedOption={checkedOption}
+            cbSubmitSetting={cbSubmitSetting}
+            label={label}
+         />);
       expect(wrapper.props().label).toEqual("Use Video");
    });
 
+   it('should have an input(checkbox)', function () {
+       const wrapper = shallow(
+         <SettingsCheckbox
+             checkedOption={checkedOption}
+             cbSubmitSetting={cbSubmitSetting}
+             label={label}
+          />);
+       expect(wrapper.find('input').length).toEqual(1);
+     });
 
+     it('cbSubmitSetting callback fires', function () {
+         const wrapper = shallow(
+           <SettingsCheckbox
+               checkedOption={checkedOption}
+               cbSubmitSetting={cbSubmitSetting}
+               label={label}
+            />);
+         expect(cbSubmitSetting.calledOnce);
+       });
+
+       it('should be true, the checkedOption value ', function () {
+           const wrapper = mount(
+             <SettingsCheckbox
+                 checkedOption={checkedOption}
+                 cbSubmitSetting={cbSubmitSetting}
+                 label={label}
+              />);
+           expect(wrapper.props().checkedOption.value).toEqual(true);
+         });
+
+         it('should equal checkedOption name ', function () {
+             const wrapper = mount(
+               <SettingsCheckbox
+                   checkedOption={checkedOption}
+                   cbSubmitSetting={cbSubmitSetting}
+                   label={label}
+                />);
+             expect(wrapper.props().checkedOption.name).toEqual("useVideo");
+           });
 
 });
