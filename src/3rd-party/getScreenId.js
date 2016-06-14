a8a1cbe7bcbbc779702ddbf55d0211788c5f19d4
jschimmoeller <jschimmoeller@schimmoeller.net>
2016-06-14 18:49:42 -0400

    share screen

--- src/3rd-party/getScreenId.js
+++ src/3rd-party/getScreenId.js
@@ -23,6 +23,7 @@ getScreenId(function (error, sourceId, screen_constraints) {
 
 (function() {
     window.getScreenId = function(callback) {
+
         // for Firefox:
         // sourceId == 'firefox'
         // screen_constraints = {...}
@@ -36,25 +37,36 @@ getScreenId(function (error, sourceId, screen_constraints) {
             return;
         }
 
-        postMessage();
+       postMessage()
 
         window.addEventListener('message', onIFrameCallback);
 
+
+
         function onIFrameCallback(event) {
-            if (!event.data) return;
+
+            if (!event.data) {
+              //console.log('DAMMM', event);
+              return;
+            }
 
             if (event.data.chromeMediaSourceId) {
                 if (event.data.chromeMediaSourceId === 'PermissionDeniedError') {
-                    callback('permission-denied');
-                } else callback(null, event.data.chromeMediaSourceId, getScreenConstraints(null, event.data.chromeMediaSourceId));
+                  window.removeEventListener('message', onIFrameCallback);
+                  callback('permission-denied');
+                } else {
+                  //console.log('haaaaaaaaaaa:::::', event);
+                  window.removeEventListener('message', onIFrameCallback);
+                  callback(null, event.data.chromeMediaSourceId, getScreenConstraints(null, event.data.chromeMediaSourceId));}
             }
 
             if (event.data.chromeExtensionStatus) {
-                callback(event.data.chromeExtensionStatus, null, getScreenConstraints(event.data.chromeExtensionStatus));
+              window.removeEventListener('message', onIFrameCallback);
+              callback(event.data.chromeExtensionStatus, null, getScreenConstraints(event.data.chromeExtensionStatus));
             }
 
             // this event listener is no more needed
-            window.removeEventListener('message', onIFrameCallback);
+            //window.removeEventListener('message', onIFrameCallback);
         }
     };
 
@@ -85,6 +97,7 @@ getScreenId(function (error, sourceId, screen_constraints) {
         }
 
         if (!iframe.isLoaded) {
+          console.log('aaaa');
             setTimeout(postMessage, 100);
             return;
         }
@@ -103,114 +116,7 @@ getScreenId(function (error, sourceId, screen_constraints) {
         iframe = document.createElement('iframe');
         iframe.onload = function() {
             iframe.isLoaded = true;
-
-            loadCallback();
-        };
-        iframe.src = 'https://www.webrtc-experiment.com/getSourceId/'; // https://wwww.yourdomain.com/getScreenId.html
-        iframe.style.display = 'none';
-        (document.body || document.documentElement).appendChild(iframe);
-    }
-
-    var iframe;
-
-    // this function is used in v3.0
-    window.getScreenConstraints = function(callback) {
-        loadIFrame(function() {
-            getScreenId(function(error, sourceId, screen_constraints) {
-                callback(error, screen_constraints.video);
-            });
-        });
-    };
-})();
-
-(function() {
-    if(document.domain.indexOf('webrtc-experiment.com') === -1) {
-        return;
-    }
-
-    window.getScreenId = function(callback) {
-        // for Firefox:
-        // sourceId == 'firefox'
-        // screen_constraints = {...}
-        if (!!navigator.mozGetUserMedia) {
-            callback(null, 'firefox', {
-                video: {
-                    mozMediaSource: 'window',
-                    mediaSource: 'window'
-                }
-            });
-            return;
-        }
-
-        postMessage();
-
-        window.addEventListener('message', onIFrameCallback);
-
-        function onIFrameCallback(event) {
-            if (!event.data) return;
-
-            if (event.data.chromeMediaSourceId) {
-                if (event.data.chromeMediaSourceId === 'PermissionDeniedError') {
-                    callback('permission-denied');
-                } else callback(null, event.data.chromeMediaSourceId, getScreenConstraints(null, event.data.chromeMediaSourceId));
-            }
-
-            if (event.data.chromeExtensionStatus) {
-                callback(event.data.chromeExtensionStatus, null, getScreenConstraints(event.data.chromeExtensionStatus));
-            }
-
-            // this event listener is no more needed
-            window.removeEventListener('message', onIFrameCallback);
-        }
-    };
-
-    function getScreenConstraints(error, sourceId) {
-        var screen_constraints = {
-            audio: false,
-            video: {
-                mandatory: {
-                    chromeMediaSource: error ? 'screen' : 'desktop',
-                    maxWidth: window.screen.width > 1920 ? window.screen.width : 1920,
-                    maxHeight: window.screen.height > 1080 ? window.screen.height : 1080
-                },
-                optional: []
-            }
-        };
-
-        if (sourceId) {
-            screen_constraints.video.mandatory.chromeMediaSourceId = sourceId;
-        }
-
-        return screen_constraints;
-    }
-
-    function postMessage() {
-        if (!iframe) {
-            loadIFrame(postMessage);
-            return;
-        }
-
-        if (!iframe.isLoaded) {
-            setTimeout(postMessage, 100);
-            return;
-        }
-
-        iframe.contentWindow.postMessage({
-            captureSourceId: true
-        }, '*');
-    }
-
-    function loadIFrame(loadCallback) {
-        if (iframe) {
-            loadCallback();
-            return;
-        }
-
-        iframe = document.createElement('iframe');
-        iframe.onload = function() {
-            iframe.isLoaded = true;
-
-            loadCallback();
+              loadCallback();
         };
         iframe.src = 'https://www.webrtc-experiment.com/getSourceId/'; // https://wwww.yourdomain.com/getScreenId.html
         iframe.style.display = 'none';
