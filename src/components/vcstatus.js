d1a23a77f77189eb7ad2ccdc51d7f3820f72a475
coryschimm <1609schimmoeller23@gmail.com>
2016-06-17 11:15:55 -0400

    #16

--- src/components/vcstatus.js
+++ src/components/vcstatus.js
@@ -2,7 +2,7 @@ import React from 'react';
 import VertoBaseComponent from './vertobasecomponent';
 import { StatusIconSVG } from './svgIcons';
 import ToolTip from './tooltip';
-
+import { FormattedMessage, injectIntl } from 'react-intl';
 
 const propTypes = {
   status: React.PropTypes.oneOf(['connected','disconnected', 'connecting', 'active']).isRequired,
@@ -38,25 +38,32 @@ class VCStatus extends VertoBaseComponent {
   }
 
   render() {
+    const { formatMessage } = this.props.intl;
     let fillColor;
+    let intlStatus;
     switch (this.props.status) {
       case 'disconnected':
         fillColor = this.getStyle('disconnectedFill');
+        intlStatus = formatMessage({"id":"DISCONNECTED", "defaultMessage":"Disconnected"});
         break;
       case 'connecting':
       case 'active':
         fillColor = this.getStyle('connectingFill');
+        intlStatus = formatMessage({"id":"CONNECTING", "defaultMessage":"Connecting"});
         break;
       case 'connected':
         fillColor = this.getStyle('connectedFill');
+        intlStatus = formatMessage({"id":"CONNECTED", "defaultMessage":"Connected"});
         break;
       default:
         fillColor = this.getDefaultStyle('disconnectedFill');
+        intlStatus = formatMessage({"id":"DISCONNECTED", "defaultMessage":"Disconnected"});
         break;
       }
 
-    //TODO internationalize  
-    const theMsg="Communication Status: "+this.props.status;
+    //TODO internationalize
+    //console.log("$$$$$$$$$$$$$$$$$$$$$$" + intlStatus);
+    const theMsg= intlStatus = formatMessage({"id":"COM_STATUS", "defaultMessage":"Communication Status: "}) + intlStatus;
 
      return (
         <ToolTip name="vcStatus" place="bottom" msg={theMsg}>
@@ -68,4 +75,4 @@ class VCStatus extends VertoBaseComponent {
 
 VCStatus.propTypes = propTypes;
 
-export default VCStatus;
+export default injectIntl(VCStatus);
