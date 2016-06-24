343eeb71d78aab676b4b9ce16152b600c171fe70
Duncan Neal <dneal@star2star.com>
2016-06-24 15:50:59 -0400

    clean up

--- src/tests/alertLog-test.js
+++ src/tests/alertLog-test.js
@@ -3,11 +3,11 @@ import { shallow, mount, render } from 'enzyme';
 import sinon from 'sinon';
 import ReactDOM from 'react-dom';
 import { mountWithIntl, shallowWithIntl } from '../helpers/intl-enzyme-test-helper.js';
-import AlertService from '../js/alertService';
+// import AlertService from '../js/alertService';
 import AlertLog from '../components/alertLog';
 import AlertLogItem from '../components/alertLogItem';
 
-jest.unmock('../js/alertService');
+// jest.unmock('../js/alertService');
 jest.unmock('../components/alertLog');
 jest.unmock('../components/alertLogItem');
 jest.unmock('../helpers/intl-enzyme-test-helper.js');
@@ -21,20 +21,21 @@ describe('Default test for AlertLog', ()=>{
     summary: "Can't hang up while sharing screen",
     detail: "You must stop sharing your screen before you can hangup the call",
     id: 0
-    };
+  };
   const sampleKey = {
     id: 0
-    };
+  };
+
 
   it('renders a modal', () => {
-    const wrapper = shallowWithIntl(<AlertLog/>);
+    const wrapper = shallowWithIntl(<AlertLog />);
      expect(wrapper.find('Modal').length).toEqual(1);
   });
 
-  it('renders a div', () => {
-    const wrapper = shallow(<AlertLog/>);
-      wrapper.setState({ alertArray: AlertService.getInstance().getAlertLog()});
-     expect(wrapper.find('div').length).toEqual(1);
+  it('renders two divs', () => {
+    const wrapper = shallow(<AlertLog />);
+
+     expect(wrapper.find('div').length).toEqual(2);
   });
 
 });
