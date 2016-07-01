aad58bf35b38828e4f432a958fb70384021df009
knicholsc <knichols@star2star.com>
2016-07-01 15:22:14 -0400

    checkbox animation beginnings...to be continued

--- src/components/settingsCheckbox.js
+++ src/components/settingsCheckbox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import VertoBaseComponent from './vertobasecomponent.js';
+import {Motion, spring} from 'react-motion';
 
 const propTypes = {
   cbSubmitSetting: React.PropTypes.func.isRequired,
@@ -11,10 +12,12 @@ const propTypes = {
   label: React.PropTypes.string.isRequired
 };
 
+const springSettings = {stiffness: 170, damping: 26};
+
 class SettingsCheckbox extends VertoBaseComponent {
   constructor(props) {
     super(props);
-    this.state={isChecked: this.props.checkedOption.value};
+    this.state={ isChecked: this.props.checkedOption.value, items: [[13,5],[13,5]], currItem: 0 };
 
     this.handleSelect = this.handleSelect.bind(this);
   }
@@ -33,35 +36,29 @@ let chkdObj = {};
     return this.props.compStyle;
   }
 
+
   getDefaultStyle(styleName) {
+
     const styles = {
       container: {
-        //display: 'flex',
-        //flexDirection: 'row',
         paddingTop: '10px',
         paddingBottom: '5px',
         position: 'relative'
       },
 
       cbcont: {
-        // animation styles
         position: 'relative',
         borderRadius: '2px',
         backgroundColor: '#fff',
         display: 'inline-block',
         height: '14px',
-        width: '14px',
-        borderBottom: '1px solid #FFF' // I don't think this is necessary
+        width: '14px'
       },
       input: {
-        //display: 'none',
         zIndex: 10,
         position: 'absolute',
         left: '-5px',
-        top: '1px',
-        //left: '-5px', // inside cbdiv
-        //top: '-7px', // inside cbdiv
-        //zIndex: 20,
+        top: '-7px',
         width: '25px',
         height: '25px',
         opacity: 0 // yooooooooooo
@@ -72,32 +69,58 @@ let chkdObj = {};
       },
       checkmark: {
         display: 'block',
-        height: '5px',
+        backgroundColor: '#fff',
+        borderRadius: '4px',
+        height: '14px',
         position: 'absolute',
-        width: '5px'
+        width: '14px'
       },
       cmafter: {
         zIndex: 1,
-        transform: 'rotate(135deg)',
-        borderRight: '3px solid #4caf50',
-        borderTop: '3px solid #4caf50',
-
-
-        //Positioning and stuff
-        content: '" "',
-        display: this.state.isChecked ? 'block' : 'none',
-        height: '5px',
-        left: '2px',
         position: 'absolute',
+        left: '2px',
         top: '0px',
-        width: '15px'
+        content: '" "',
+        transform: 'rotate(135deg)',
+        borderRight: this.state.isChecked ? '3px solid #4caf50' : 'none',
+        borderTop: this.state.isChecked ? '3px solid #4caf50' : 'none',
+        height: this.state.isChecked ? '5px' : '0px',
+        width: this.state.isChecked ? '13px' : '0px',
+        transition : 'width .5s ease, height .5s ease',
+        //animation: 'x .5s ease',
+        //animationName: checkoff,
+      },
+      emptyDiv: {
+
       }
     };
 
     return (styles[styleName]);
   }
 
+
+
   render() {
+
+
+
+    // Sliding Animation Styles
+      // const {items, currItem} = this.state;
+      // const [currWidth, currHeight] = items[currItem];
+      // const widths = items.map(([origW, origH])=> currHeight / origH * origW);
+      // const leftStartCoords = widths
+      //   .slice(0, currItem)
+      //   .reduce((sum,width) => sum - width, 0);
+      //
+      // let configs = [];
+      // items.reduce((prevLeft, [origW, origH], i) => {
+      //   configs.push({
+      //     left: spring(prevLeft, springSettings),
+      //     height: spring(currHeight, springSettings),
+      //     width: spring(widths[i], springSettings)
+      //   });
+      //   return prevLeft + widths[i];
+      // }, leftStartCoords);
 
     return (
       <div
@@ -105,10 +128,18 @@ let chkdObj = {};
       >
         <div
             className="cbcont"
-            ref="checkMe"
-            checked={this.props.checkedOption.value}
             style={{...this.getDefaultStyle('cbcont')}}
         >
+          <input
+              ref="checkMe"
+              type="checkbox"
+              style={{...this.getDefaultStyle('input')}}
+              onChange={()=>{
+                this.handleSelect();
+                setTimeout(()=>this.setState({...this.state, isChecked: !this.state.isChecked}),0);
+              }}
+              checked={this.props.checkedOption.value}
+          />
           <label
               className="checkmark"
               style={{...this.getDefaultStyle('checkmark')}}
@@ -120,16 +151,6 @@ let chkdObj = {};
               </span>
           </label>
         </div>
-        <input
-            ref="checkMe"
-            type="checkbox"
-            style={{...this.getDefaultStyle('input')}}
-            onChange={()=>{
-              this.handleSelect();
-              setTimeout(()=>this.setState({...this.state, isChecked: !this.state.isChecked}),0);
-            }}
-            checked={this.props.checkedOption.value}
-        />
         <span
             style={{...this.getStyle('label')}}
         >
