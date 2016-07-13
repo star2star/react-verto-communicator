d4867f534507f3b847db50f5bab1e8c568b9ff78
jschimmoeller <jschimmoeller@schimmoeller.net>
2016-07-13 15:06:06 -0400

    build issues for dist

--- src/js/whitelabelData.js
+++ src/js/whitelabelData.js
@@ -5,7 +5,7 @@ const whitelabelData = {
   "chromeDownloadURL"       : "https://www.google.com/chrome/browser/desktop/",
   "firefoxDownloadURL"      : "https://www.mozilla.org/en-US/firefox/new",
   "safariDownloadURL"       : "https://support.apple.com/downloads/safari",
-  "logoSrc"                 : "../img/Verto-logo.png",
-  "poweredBy"               : "../img/FreeSwitch-Logo.png"
+  "logoSrc"                 : "./img/Verto-logo.png",
+  "poweredBy"               : "./img/FreeSwitch-Logo.png"
 };
 module.exports = whitelabelData;
--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,8 @@ if (process.env.NODE_ENV === 'production') {
           //{ from: 'messages', to: 'messages' },
           { from: 'src/index.html', to: 'index.html' },
           { from: 'src/css', to: 'css'},
-          { from: 'src/img', to: 'img' }, 
+          { from: 'src/img', to: 'img' },
+          { from: 'images', to: 'img' }, 
           { from: 'src/locales', to: 'locales'},
           { from: 'src/3rd-party', to: '3rd-party'},
           { from: 'src/favicon.ico', to: 'favicon.ico' }
